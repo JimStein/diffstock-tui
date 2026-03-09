ba64bd821582da54b922c8666d08d3837c7d3b89
JimStein <mr.leong@163.com>
2026-03-09 09:59:22 +0800

    1

---
+++ target_featurecheck/release/deps/itoa-a4b7999ee0fa2d37.d
@@ -0,0 +1,8 @@
+D:\Users\mrleo\Documents\GitHub\diffstock-tui\target_featurecheck\release\deps\itoa-a4b7999ee0fa2d37.d: C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\itoa-1.0.15\src\lib.rs C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\itoa-1.0.15\src\udiv128.rs
+
+D:\Users\mrleo\Documents\GitHub\diffstock-tui\target_featurecheck\release\deps\libitoa-a4b7999ee0fa2d37.rlib: C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\itoa-1.0.15\src\lib.rs C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\itoa-1.0.15\src\udiv128.rs
+
+D:\Users\mrleo\Documents\GitHub\diffstock-tui\target_featurecheck\release\deps\libitoa-a4b7999ee0fa2d37.rmeta: C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\itoa-1.0.15\src\lib.rs C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\itoa-1.0.15\src\udiv128.rs
+
+C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\itoa-1.0.15\src\lib.rs:
+C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\itoa-1.0.15\src\udiv128.rs:
---
+++ target_featurecheck/release/deps/zeroize-a3406b2af534442b.d
@@ -0,0 +1,8 @@
+D:\Users\mrleo\Documents\GitHub\diffstock-tui\target_featurecheck\release\deps\zeroize-a3406b2af534442b.d: C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zeroize-1.8.2\src\lib.rs C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zeroize-1.8.2\src\x86.rs
+
+D:\Users\mrleo\Documents\GitHub\diffstock-tui\target_featurecheck\release\deps\libzeroize-a3406b2af534442b.rlib: C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zeroize-1.8.2\src\lib.rs C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zeroize-1.8.2\src\x86.rs
+
+D:\Users\mrleo\Documents\GitHub\diffstock-tui\target_featurecheck\release\deps\libzeroize-a3406b2af534442b.rmeta: C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zeroize-1.8.2\src\lib.rs C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zeroize-1.8.2\src\x86.rs
+
+C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zeroize-1.8.2\src\lib.rs:
+C:\Users\mrleo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zeroize-1.8.2\src\x86.rs:
